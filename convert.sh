d9054d6a086bf25e1d335c4a64849c644eec593f
Alan Hardman <alanaktion@gmail.com>
2017-11-26 19:50:14 -0700

    Adding custom images for additional Xfwm buttons

--- convert.sh
+++ convert.sh
@@ -22,27 +22,53 @@ bgactive='#709080'
 bginactive='#363636'
 function asset2xwfm ()
 {
-  convert "assets/$1.png" -background "$3" -alpha remove "xfwm4/$2.xpm"
+  convert "assets/titlebutton-$1.png" -background "$3" -alpha remove "xfwm4/$2.xpm"
 }
-asset2xwfm titlebutton-close close-active "$bgactive"
-asset2xwfm titlebutton-close-hover close-prelight "$bgactive"
-asset2xwfm titlebutton-close-active close-pressed "$bgactive"
-asset2xwfm titlebutton-close-backdrop close-inactive "$bginactive"
 
-asset2xwfm titlebutton-maximize maximize-active "$bgactive"
-asset2xwfm titlebutton-maximize-hover maximize-prelight "$bgactive"
-asset2xwfm titlebutton-maximize-active maximize-pressed "$bgactive"
-asset2xwfm titlebutton-maximize-backdrop maximize-inactive "$bginactive"
+asset2xwfm close close-active "$bgactive"
+asset2xwfm close-hover close-prelight "$bgactive"
+asset2xwfm close-active close-pressed "$bgactive"
+asset2xwfm close-backdrop close-inactive "$bginactive"
 
-asset2xwfm titlebutton-minimize hide-active "$bgactive"
-asset2xwfm titlebutton-minimize-hover hide-prelight "$bgactive"
-asset2xwfm titlebutton-minimize-active hide-pressed "$bgactive"
-asset2xwfm titlebutton-minimize-backdrop hide-inactive "$bginactive"
+asset2xwfm maximize maximize-active "$bgactive"
+asset2xwfm maximize-hover maximize-prelight "$bgactive"
+asset2xwfm maximize-active maximize-pressed "$bgactive"
+asset2xwfm maximize-backdrop maximize-inactive "$bginactive"
 
-asset2xwfm titlebutton-maximize-maximized maximize-toggled-active "$bgactive"
-asset2xwfm titlebutton-maximize-maximized-hover maximize-toggled-prelight "$bgactive"
-asset2xwfm titlebutton-maximize-maximized-active maximize-toggled-pressed "$bgactive"
-asset2xwfm titlebutton-maximize-maximized-backdrop maximize-toggled-inactive "$bginactive"
+asset2xwfm minimize hide-active "$bgactive"
+asset2xwfm minimize-hover hide-prelight "$bgactive"
+asset2xwfm minimize-active hide-pressed "$bgactive"
+asset2xwfm minimize-backdrop hide-inactive "$bginactive"
+
+asset2xwfm maximize-maximized maximize-toggled-active "$bgactive"
+asset2xwfm maximize-maximized-hover maximize-toggled-prelight "$bgactive"
+asset2xwfm maximize-maximized-active maximize-toggled-pressed "$bgactive"
+asset2xwfm maximize-maximized-backdrop maximize-toggled-inactive "$bginactive"
+
+function xfwm2xpm ()
+{
+  convert "xfwm4/src/$1.png" -background "$3" -alpha remove "xfwm4/$2.xpm"
+}
+
+xfwm2xpm shade shade-active "$bgactive"
+xfwm2xpm shade-hover shade-prelight "$bgactive"
+xfwm2xpm shade-active shade-pressed "$bgactive"
+xfwm2xpm shade-backdrop shade-inactive "$bginactive"
+
+xfwm2xpm shade-toggled shade-toggled-active "$bgactive"
+xfwm2xpm shade-toggled-hover shade-toggled-prelight "$bgactive"
+xfwm2xpm shade-toggled-active shade-toggled-pressed "$bgactive"
+xfwm2xpm shade-toggled-backdrop shade-toggled-inactive "$bginactive"
+
+xfwm2xpm stick stick-active "$bgactive"
+xfwm2xpm stick-hover stick-prelight "$bgactive"
+xfwm2xpm stick-active stick-pressed "$bgactive"
+xfwm2xpm stick-backdrop stick-inactive "$bginactive"
+
+xfwm2xpm stick-toggled stick-toggled-active "$bgactive"
+xfwm2xpm stick-toggled-hover stick-toggled-prelight "$bgactive"
+xfwm2xpm stick-toggled-active stick-toggled-pressed "$bgactive"
+xfwm2xpm stick-toggled-backdrop stick-toggled-inactive "$bginactive"
 
 # Update primary colors in theme files
 sed -i -e 's/3daee9/708080/g' gtk-2.0/gtkrc
